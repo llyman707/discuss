6450303bf17df55f269458b734a587eaf3677e39
llyman707 <llyman707@gmail.com>
2025-10-19 18:11:39 -0400

    creating server actions and buttons

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,15 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
+
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
