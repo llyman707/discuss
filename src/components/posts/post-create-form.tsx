992b38a5fa6afd48571b2b96c0cce9ee1155618b
llyman707 <llyman707@gmail.com>
2025-10-21 08:08:43 -0400

    adding more redirect functionality

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -12,9 +12,13 @@ import {
 import * as actions from '@/actions';
 import FormButton from '@/components/common/form-button';
 
-export default function PostCreateForm() {
-    const [formState, action, isPending] = useActionState(actions.createTopic, {
-        errors: {}
+interface PostCreateFormProps{
+    slug: string;
+}
+
+export default function PostCreateForm({slug} : PostCreateFormProps) {
+    const [formState, action, isPending] = useActionState(actions.createPost.bind(null,slug), {
+        errors: {},
     });
     return (
         <Popover placement="left">
@@ -27,7 +31,11 @@ export default function PostCreateForm() {
                         <h3 className="text-lg">Create a Post</h3>
                         <Input isInvalid={!!formState.errors.title} errorMessage={formState.errors.title?.join(',')} name="title" label="Title" labelPlacement="outside" placeholder="Title" />
                         <Textarea isInvalid={!!formState.errors.content} errorMessage={formState.errors.content?.join(',')} name="content" label="Content" labelPlacement="outside" placeholder="Content" />
-                        
+
+                        {
+                            formState.errors._form ? (<div className="rounded p-2 bg-red-200 border boreder-red-400"> {formState.errors._form.join(',')}</div>) : null
+                        }
+
                         <FormButton isLoading={isPending}>
                             Create Post
                         </FormButton>
