e701671904874be4b9b2629d6b13b1cb126b0f20
llyman707 <llyman707@gmail.com>
2025-10-25 14:29:23 -0400

    adding comment functionality and search features to app

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,11 +1,15 @@
 import type { Comment } from '@prisma/client';
+import {cache} from 'react';
 import { db } from '@/db';
 
 export type CommentWithAuthor = Comment & {
     user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor[]>{
+export const fetchCommentsByPostId = cache(
+    (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log('Making a query!');
+
     return db.comment.findMany({
         where: { postId },
         include: {
@@ -18,3 +22,4 @@ export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor
         },
     });
 }
+);
